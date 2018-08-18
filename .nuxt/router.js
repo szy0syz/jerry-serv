884b1904475012af6aa0fbdbd9834d5586bb4b28
szy0syz <szy0syz@gmail.com>
2018-08-18 19:36:51 +0800

    ☀️ Update  [FE]  更新ArticleList展示配置

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -4,26 +4,25 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 const _665b254c = () => import('../pages/login.vue' /* webpackChunkName: "pages/login" */).then(m => m.default || m)
+const _e010cd0e = () => import('../pages/error-page/500.vue' /* webpackChunkName: "pages/error-page/500" */).then(m => m.default || m)
+const _2c479d07 = () => import('../pages/system/role.vue' /* webpackChunkName: "pages/system/role" */).then(m => m.default || m)
+const _023ad900 = () => import('../pages/tables/editable-table.vue' /* webpackChunkName: "pages/tables/editable-table" */).then(m => m.default || m)
+const _44dcfc40 = () => import('../pages/error-page/error-page.vue' /* webpackChunkName: "pages/error-page/error-page" */).then(m => m.default || m)
 const _e8eb059e = () => import('../pages/article/tags.vue' /* webpackChunkName: "pages/article/tags" */).then(m => m.default || m)
 const _5b49bc3c = () => import('../pages/error-page/404.vue' /* webpackChunkName: "pages/error-page/404" */).then(m => m.default || m)
-const _023ad900 = () => import('../pages/tables/editable-table.vue' /* webpackChunkName: "pages/tables/editable-table" */).then(m => m.default || m)
-const _787ef56c = () => import('../pages/extended/nav.vue' /* webpackChunkName: "pages/extended/nav" */).then(m => m.default || m)
-const _dbc5087a = () => import('../pages/system/log.vue' /* webpackChunkName: "pages/system/log" */).then(m => m.default || m)
-const _2c479d07 = () => import('../pages/system/role.vue' /* webpackChunkName: "pages/system/role" */).then(m => m.default || m)
-const _014ae23f = () => import('../pages/extended/swiper.vue' /* webpackChunkName: "pages/extended/swiper" */).then(m => m.default || m)
-const _083b2f36 = () => import('../pages/article/list.vue' /* webpackChunkName: "pages/article/list" */).then(m => m.default || m)
-const _e093405c = () => import('../pages/article/type.vue' /* webpackChunkName: "pages/article/type" */).then(m => m.default || m)
-const _2c4955dc = () => import('../pages/system/user.vue' /* webpackChunkName: "pages/system/user" */).then(m => m.default || m)
-const _7ce750c8 = () => import('../pages/form/preview.vue' /* webpackChunkName: "pages/form/preview" */).then(m => m.default || m)
 const _5b3ba4bb = () => import('../pages/error-page/403.vue' /* webpackChunkName: "pages/error-page/403" */).then(m => m.default || m)
-const _dd878c94 = () => import('../pages/release/activity.vue' /* webpackChunkName: "pages/release/activity" */).then(m => m.default || m)
 const _8c25f5b2 = () => import('../pages/article/publish.vue' /* webpackChunkName: "pages/article/publish" */).then(m => m.default || m)
-const _66b45c5b = () => import('../pages/article/management.vue' /* webpackChunkName: "pages/article/management" */).then(m => m.default || m)
-const _44dcfc40 = () => import('../pages/error-page/error-page.vue' /* webpackChunkName: "pages/error-page/error-page" */).then(m => m.default || m)
-const _670fe355 = () => import('../pages/release/info.vue' /* webpackChunkName: "pages/release/info" */).then(m => m.default || m)
-const _e010cd0e = () => import('../pages/error-page/500.vue' /* webpackChunkName: "pages/error-page/500" */).then(m => m.default || m)
+const _dbc5087a = () => import('../pages/system/log.vue' /* webpackChunkName: "pages/system/log" */).then(m => m.default || m)
 const _40d3db84 = () => import('../pages/form/work-flow.vue' /* webpackChunkName: "pages/form/work-flow" */).then(m => m.default || m)
+const _7ce750c8 = () => import('../pages/form/preview.vue' /* webpackChunkName: "pages/form/preview" */).then(m => m.default || m)
+const _dd878c94 = () => import('../pages/release/activity.vue' /* webpackChunkName: "pages/release/activity" */).then(m => m.default || m)
+const _787ef56c = () => import('../pages/extended/nav.vue' /* webpackChunkName: "pages/extended/nav" */).then(m => m.default || m)
+const _2c4955dc = () => import('../pages/system/user.vue' /* webpackChunkName: "pages/system/user" */).then(m => m.default || m)
+const _014ae23f = () => import('../pages/extended/swiper.vue' /* webpackChunkName: "pages/extended/swiper" */).then(m => m.default || m)
 const _0a3fc6aa = () => import('../pages/tables/dragable-table.vue' /* webpackChunkName: "pages/tables/dragable-table" */).then(m => m.default || m)
+const _e093405c = () => import('../pages/article/type.vue' /* webpackChunkName: "pages/article/type" */).then(m => m.default || m)
+const _083b2f36 = () => import('../pages/article/list.vue' /* webpackChunkName: "pages/article/list" */).then(m => m.default || m)
+const _670fe355 = () => import('../pages/release/info.vue' /* webpackChunkName: "pages/release/info" */).then(m => m.default || m)
 const _032aca43 = () => import('../pages/index.vue' /* webpackChunkName: "pages/index" */).then(m => m.default || m)
 
 
@@ -90,6 +89,26 @@ export function createRouter () {
 			name: "login"
 		},
 		{
+			path: "/error-page/500",
+			component: _e010cd0e,
+			name: "error-page-500"
+		},
+		{
+			path: "/system/role",
+			component: _2c479d07,
+			name: "system-role"
+		},
+		{
+			path: "/tables/editable-table",
+			component: _023ad900,
+			name: "tables-editable-table"
+		},
+		{
+			path: "/error-page/error-page",
+			component: _44dcfc40,
+			name: "error-page-error-page"
+		},
+		{
 			path: "/article/tags",
 			component: _e8eb059e,
 			name: "article-tags"
@@ -100,59 +119,9 @@ export function createRouter () {
 			name: "error-page-404"
 		},
 		{
-			path: "/tables/editable-table",
-			component: _023ad900,
-			name: "tables-editable-table"
-		},
-		{
-			path: "/extended/nav",
-			component: _787ef56c,
-			name: "extended-nav"
-		},
-		{
-			path: "/system/log",
-			component: _dbc5087a,
-			name: "system-log"
-		},
-		{
-			path: "/system/role",
-			component: _2c479d07,
-			name: "system-role"
-		},
-		{
-			path: "/extended/swiper",
-			component: _014ae23f,
-			name: "extended-swiper"
-		},
-		{
-			path: "/article/list",
-			component: _083b2f36,
-			name: "article-list"
-		},
-		{
-			path: "/article/type",
-			component: _e093405c,
-			name: "article-type"
-		},
-		{
-			path: "/system/user",
-			component: _2c4955dc,
-			name: "system-user"
-		},
-		{
-			path: "/form/preview",
-			component: _7ce750c8,
-			name: "form-preview"
-		},
-		{
 			path: "/error-page/403",
 			component: _5b3ba4bb,
 			name: "error-page-403"
-		},
-		{
-			path: "/release/activity",
-			component: _dd878c94,
-			name: "release-activity"
 		},
 		{
 			path: "/article/publish",
@@ -160,24 +129,9 @@ export function createRouter () {
 			name: "article-publish"
 		},
 		{
-			path: "/article/management",
-			component: _66b45c5b,
-			name: "article-management"
-		},
-		{
-			path: "/error-page/error-page",
-			component: _44dcfc40,
-			name: "error-page-error-page"
-		},
-		{
-			path: "/release/info",
-			component: _670fe355,
-			name: "release-info"
-		},
-		{
-			path: "/error-page/500",
-			component: _e010cd0e,
-			name: "error-page-500"
+			path: "/system/log",
+			component: _dbc5087a,
+			name: "system-log"
 		},
 		{
 			path: "/form/work-flow",
@@ -185,9 +139,49 @@ export function createRouter () {
 			name: "form-work-flow"
 		},
 		{
+			path: "/form/preview",
+			component: _7ce750c8,
+			name: "form-preview"
+		},
+		{
+			path: "/release/activity",
+			component: _dd878c94,
+			name: "release-activity"
+		},
+		{
+			path: "/extended/nav",
+			component: _787ef56c,
+			name: "extended-nav"
+		},
+		{
+			path: "/system/user",
+			component: _2c4955dc,
+			name: "system-user"
+		},
+		{
+			path: "/extended/swiper",
+			component: _014ae23f,
+			name: "extended-swiper"
+		},
+		{
 			path: "/tables/dragable-table",
 			component: _0a3fc6aa,
 			name: "tables-dragable-table"
+		},
+		{
+			path: "/article/type",
+			component: _e093405c,
+			name: "article-type"
+		},
+		{
+			path: "/article/list",
+			component: _083b2f36,
+			name: "article-list"
+		},
+		{
+			path: "/release/info",
+			component: _670fe355,
+			name: "release-info"
 		},
 		{
 			path: "/",
--- pages/article/list.vue
+++ pages/article/list.vue
@@ -1,19 +1,216 @@
 <style lang="less">
 @import '../../assets/css/common.less';
+.ivu-table-row-highlight {
+  background-color: #ffffcc;
+}
+
+.main-con {
+  background-color: #fff;
+}
+
+.filter-con {
+  height: 60px;
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  .filter-group {
+    margin-left: 16px;
+  }
+
+  .search-font {
+    font-size: 14px;
+    font-weight: 700;
+  }
+
+  .optBtns {
+    margin-right: 18px;
+    & button {
+      margin-left: 8px;
+    }
+  }
+}
 </style>
 
 <template>
-  <div>
-    <h1>article-list</h1>
+  <div class="main-con">
+    <Row style="border-bottom: 1px dashed #ddd;">
+      <Col span="14">
+      <div class="filter-con">
+        <div class="filter-group">
+          <span class="search-font">文档标题：&nbsp;</span>
+          <Input v-model="inpNumber" placeholder="检索文档标题" clearable style="width: 160px"></Input>
+        </div>
+        <div class="filter-group">
+          <span class="search-font">文档内容：&nbsp;</span>
+          <Input v-model="inpName" placeholder="检索文档内容" clearable style="width: 260px"></Input>
+        </div>
+        <div class="filter-group">
+          <Button @click="handleSearch" type="primary" icon="ios-search">搜索</Button>
+        </div>
+      </div>
+      </Col>
+      <Col span="10">
+      <div class="filter-con" style="justify-content: flex-end;">
+        <div class="optBtns">
+          <Button @click="handleRow('add')" type="info">添加</Button>
+          <Button @click="handleRow('edit')" type="warning">修改</Button>
+          <Button @click="handleRemoveRow" type="error">删除</Button>
+          <Button type="primary">审核</Button>
+        </div>
+      </div>
+      </Col>
+    </Row>
+    <Table highlight-row @on-current-change="handleCurrentRow" style="margin-top:10px;" stripe border :height="700" :columns="columns" :data="unitList"></Table>
+    <Modal @on-ok="handleOpt" width="700" v-model="showForm" title="单位登记变更">
+      <Card>
+        <Form :model="rowModel" :label-width="90">
+          <FormItem label="单位编号： ">
+            <Input v-model="rowModel.number" placeholder="请输入单位编号"></Input>
+          </FormItem>
+          <FormItem label="单位名称： ">
+            <Input v-model="rowModel.name" placeholder="请输入单位名称"></Input>
+          </FormItem>
+          <FormItem label="单位地址： ">
+            <Input v-model="rowModel.address" placeholder="请输入单位地址"></Input>
+          </FormItem>
+          <FormItem label="单位地址： ">
+            <div style="width:600px;">
+              <al-selector v-model="selectedArea" data-type="code" level="2" />
+            </div>
+          </FormItem>
+        </Form>
+      </Card>
+    </Modal>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'article-list',
+  name: 'unitReg',
   data() {
-    return {}
+    return {
+      optType: 'add',
+      selectedArea: [],
+      showForm: false,
+      currentRow: '',
+      inpNumber: '',
+      inpName: '',
+      rowModel: {
+        _id: '',
+        name: '',
+        number: '',
+        address: '',
+        region: 0
+      },
+      columns: [
+        {
+          title: '序号',
+          type: 'index',
+          width: 68,
+          align: 'center'
+        },
+        {
+          title: '置顶',
+          key: 'isTop',
+          width: 70,
+          align: 'center'
+        },
+        {
+          title: '标题',
+          key: 'title'
+        },
+        {
+          title: '创建时间',
+          render: (h, params) => {
+            return h('span', params.row.meta.createdAt)
+          }
+        },
+        {
+          title: '文档类别',
+          render: (h, params) => {
+            return h('span', params.row.type.name)
+          }
+        },
+        {
+          title: '点击量',
+          key: 'clickNum'
+        },
+        {
+          title: '点赞数',
+          key: 'likeNum'
+        },
+        {
+          title: '评论数',
+          key: 'commentNum'
+        },
+        {
+          title: '作者',
+          render: (h, params) => {
+            return h('span', params.row.author.name)
+          }
+        }
+      ]
+    }
   },
-  mounted() {}
+  computed: {
+    // unitList() {
+    //   return this.$store.state.project.projUnits
+    // }
+  },
+  methods: {
+    async handleOpt() {
+      // this.rowModel.region = this.selectedArea[2]
+      // if (this.optType === 'add') {
+      //   await this.$store.dispatch('postProjUnit', this.rowModel)
+      // } else {
+      //   console.log('edit')
+      //   await this.$store.dispatch('putProjUnit', this.rowModel)
+      // }
+    },
+    handleCurrentRow(curt, prev) {
+      this.currentRow = curt
+    },
+    handleRow(type) {
+      // this.optType = type
+      // if (type === 'add') {
+      //   this.rowModel = {
+      //     name: '',
+      //     number: '',
+      //     address: '',
+      //     region: 0
+      //   }
+      //   this.showForm = true
+      // } else {
+      //   if (!this.currentRow) {
+      //     this.$Message.error('请点击需编辑的行')
+      //   } else {
+      //     this.rowModel._id = this.currentRow._id
+      //     this.rowModel.number = this.currentRow.number
+      //     this.rowModel.name = this.currentRow.name
+      //     this.rowModel.address = this.currentRow.address
+      //     this.rowModel.region = this.currentRow.region
+      //     this.showForm = true
+      //   }
+      // }
+    },
+    handleRemoveRow() {
+      // if (!this.currentRow) {
+      //   this.$Message.error('请点击需要删除的行')
+      //   return void 0
+      // }
+      // this.$store.dispatch('removeProjUnit', this.currentRow._id)
+    },
+    handleSearch() {
+      // this.$store.dispatch('fetchProjUnit', {
+      //   number: this.inpNumber,
+      //   name: this.inpName
+      // })
+    }
+  },
+  mounted() {
+    // this.$store.dispatch('getProjUnit')
+  }
 }
 </script>
